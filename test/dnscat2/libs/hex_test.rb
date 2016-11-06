df71ec20e2b84ab2b7ff65e99ac828c5e82f2ea6
iagox86 <ron-git@skullsecurity.org>
2016-11-06 13:52:58 -0800

    Reverse the conditions in the assert() calls in test_hex to match the order they're supposed to be

--- test/dnscat2/libs/hex_test.rb
+++ test/dnscat2/libs/hex_test.rb
@@ -5,54 +5,54 @@ require 'dnscat2/core/libs/hex'
 class Dnscat2::Core::HexTestToS < Test::Unit::TestCase
   def test_empty_string()
     str = Dnscat2::Core::Libs::Hex.to_s('')
-    assert_equal(str, '')
+    assert_equal('', str)
   end
 
   def test_one_character_string()
     str = Dnscat2::Core::Libs::Hex.to_s('A')
-    assert_equal(str, '00000000  41                                                A')
+    assert_equal('00000000  41                                                A', str)
   end
 
   def test_sixteen_character_string()
     str = Dnscat2::Core::Libs::Hex.to_s('AAAAAAAAAAAAAAAA')
-    assert_equal(str, '00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA')
+    assert_equal('00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA', str)
   end
 
   def test_more_than_sixteen()
     str = Dnscat2::Core::Libs::Hex.to_s('AAAAAAAAAAAAAAAAAAAA')
-    assert_equal(str, "00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
-                      "00000010  41 41 41 41                                       AAAA")
+    assert_equal("00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
+                 "00000010  41 41 41 41                                       AAAA", str)
   end
 
   def test_null_bytes()
     str = Dnscat2::Core::Libs::Hex.to_s("A\0A\0A")
-    assert_equal(str, '00000000  41 00 41 00 41                                    A.A.A')
+    assert_equal('00000000  41 00 41 00 41                                    A.A.A', str)
   end
 
   def test_newlines()
     str = Dnscat2::Core::Libs::Hex.to_s("A\nA\nA")
-    assert_equal(str, '00000000  41 0a 41 0a 41                                    A.A.A')
+    assert_equal('00000000  41 0a 41 0a 41                                    A.A.A', str)
   end
 
   def test_exactly_thirty_two()
     str = Dnscat2::Core::Libs::Hex.to_s('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-    assert_equal(str, "00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
-                      "00000010  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA")
+    assert_equal("00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
+                 "00000010  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA", str)
   end
 
   def test_indent()
     str = Dnscat2::Core::Libs::Hex.to_s('A', indent: 1)
-    assert_equal(str, ' 00000000  41                                                A')
+    assert_equal(' 00000000  41                                                A', str)
   end
 
   def test_indent_two_lines()
     str = Dnscat2::Core::Libs::Hex.to_s('AAAAAAAAAAAAAAAAAAAA', indent: 2)
-    assert_equal(str, "  00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
-                      "  00000010  41 41 41 41                                       AAAA")
+    assert_equal("  00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA\n" +
+                 "  00000010  41 41 41 41                                       AAAA", str)
   end
 
   def test_indent_with_sixteen_characters()
     str = Dnscat2::Core::Libs::Hex.to_s('AAAAAAAAAAAAAAAA', indent: 2)
-    assert_equal(str, '  00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA')
+    assert_equal('  00000000  41 41 41 41 41 41 41 41 41 41 41 41 41 41 41 41   AAAAAAAAAAAAAAAA', str)
   end
 end
