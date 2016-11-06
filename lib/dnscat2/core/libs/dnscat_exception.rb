f1c9b200d04452f4a0a44d76fb760bceb79a4ac1
iagox86 <ron-git@skullsecurity.org>
2016-11-06 13:22:51 -0800

    Added dnscat_exception.rb from the original project

---
+++ lib/dnscat2/core/libs/dnscat_exception.rb
@@ -0,0 +1,18 @@
+##
+# dnscat_exception.rb
+# Created July 1, 2013 (Canada Day!)
+# By Ron Bowes
+#
+# See LICENSE.md
+#
+# Implements a simple exception class for dnscat2 protocol errors.
+##
+
+module Dnscat2
+  module Core
+    module Libs
+      class DnscatException < StandardError
+      end
+    end
+  end
+end
