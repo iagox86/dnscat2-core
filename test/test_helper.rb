e40ee6475b1099df33fb28d27500489c72bac170
iagox86 <ron-git@skullsecurity.org>
2016-11-06 13:09:14 -0800

    Added prettier output to the MiniTests gem

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dnscat2/core'
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+
+reporter_options = { color: true }
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(reporter_options)]
+
