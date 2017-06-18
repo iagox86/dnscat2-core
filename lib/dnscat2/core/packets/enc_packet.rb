e1002f6aa6051f7d7fd920f9f567ac267b87f8eb
iagox86 <ron-git@skullsecurity.org>
2017-06-18 12:30:53 -0700

    Wrote tests for EncPacketInit

--- lib/dnscat2/core/packets/enc_packet.rb
+++ lib/dnscat2/core/packets/enc_packet.rb
@@ -6,92 +6,115 @@
 # See: LICENSE.md
 ##
 
+require 'dnscat2/core/libs/crypto_helper'
+require 'dnscat2/core/packets/packet_constants'
+require 'dnscat2/core/packets/packet_helper'
+
 module Dnscat2
   module Core
     module Packets
-      class EncPacket
-        attr_reader :subtype, :flags
-        attr_reader :public_key_x, :public_key_y # SUBTYPE_INIT
-        attr_reader :authenticator # SUBTYPE_AUTH
+      class EncPacketInit
+        extend PacketHelper
+        attr_reader :public_key_x, :public_key_y
 
-        def initialize(params = {})
-          @subtype = params[:subtype] || raise(ArgumentError, "params[:subtype] is required!")
-          @flags   = params[:flags]   || raise(ArgumentError, "params[:flags] is required!")
-
-          if(@subtype == SUBTYPE_INIT)
-            @public_key_x = params[:public_key_x] || raise(ArgumentError, "params[:public_key_x] is required!")
-            @public_key_y = params[:public_key_y] || raise(ArgumentError, "params[:public_key_y] is required!")
-
-            if(!@public_key_x.is_a?(Bignum) || !@public_key_y.is_a?(Bignum))
-              raise(ArgumentError, "Public keys have to be Bignums! (Seen: #{@public_key_x.class} #{@public_key_y.class})")
-            end
-          elsif(@subtype == SUBTYPE_AUTH)
-            @authenticator = params[:authenticator] || raise(ArgumentError, "params[:authenticator] is required!")
-
-            if(@authenticator.length != 32)
-              raise(ArgumentError, "params[:authenticator] was the wrong size!")
-            end
-          else
-            raise(ArgumentError, "Unknown subtype: #{@subtype}")
-          end
+        def initialize(public_key_x:, public_key_y:)
+          @public_key_x = public_key_x
+          @public_key_y = public_key_y
         end
 
-        def EncBody.parse(data)
-          at_least?(data, 4) || raise(DnscatException, "ENC packet is too short!")
+        def self.parse(data)
+          exactly?(data, 64)
+          public_key_x, public_key_y = data.unpack("a32a32")
+
+          return self.new(
+            public_key_x: CryptoHelper.binary_to_bignum(public_key_x),
+            public_key_y: CryptoHelper.binary_to_bignum(public_key_y),
+          )
+        end
+
+        def to_bytes()
+          public_key_x = CryptoHelper.bignum_to_binary(@public_key_x)
+          public_key_y = CryptoHelper.bignum_to_binary(@public_key_y)
+
+          return [public_key_x, public_key_y].pack("a32a32")
+        end
+
+        def to_s()
+          return "[[INIT]] :: pubkey = 0x%s,0x%s" % [CryptoHelper.bignum_to_text(@public_key_x), CryptoHelper.bignum_to_text(@public_key_y)]
+        end
+      end
+
+      class EncPacketAuth
+        extend PacketHelper
+        attr_reader :authenticator
+
+        def initialize(authenticator:)
+          exactly?(authenticator, 32)
+          @authenticator = authenticator
+        end
+
+        def self.parse(data)
+          authenticator, data = data.unpack("a32a*")
+          exactly?(data, 0)
+
+          return self.new(
+            authenticator: authenticator,
+          )
+        end
+
+        def to_bytes()
+          return [@authenticator].pack("a32")
+        end
+
+        def to_s()
+          return "[[AUTH]] :: authenticator = %s" % [@authenticator.unpack("H*").pop()]
+        end
+      end
+
+      class EncPacket
+        # This gives us the verify_* functions
+        extend PacketHelper
+
+        attr_reader :subtype, :flags
+
+        def initialize(subtype:, flags:, body:)
+          if ![SUBTYPE_INIT, SUBTYPE_AUTH].include?(subtype)
+            raise(DnscatException, "Illegal subtype on ENC packet")
+          end
+
+          @subtype = subtype
+          @flags   = flags
+          @body    = body
+        end
+
+        def self.parse(data)
+          at_least?(data, 4)
 
           subtype, flags, data = data.unpack("nna*")
 
-          params = {
-            :subtype => subtype,
-            :flags   => flags,
-          }
-
           if(subtype == SUBTYPE_INIT)
-            exactly?(data, 64) || raise(DnscatException, "ENC packet is too short!")
-
-            public_key_x, public_key_y, data = data.unpack("a32a32a*")
-
-            params[:public_key_x] = CryptoHelper.binary_to_bignum(public_key_x)
-            params[:public_key_y] = CryptoHelper.binary_to_bignum(public_key_y)
-
+            exactly?(data, 64)
+            body = EncPacketInit.parse(data)
           elsif(subtype == SUBTYPE_AUTH)
-            exactly?(data, 32) || raise(DnscatException, "ENC packet is too short!")
-
-            authenticator, data = data.unpack("a32a*")
-
-            params[:authenticator] = authenticator
+            exactly?(data, 32)
+            body = EncPacketAuth.parse(data)
           else
             raise(DnscatException, "Unknown subtype: #{subtype}")
           end
 
-          if(data != "")
-            raise(DnscatException, "Extra data on the end of an ENC packet")
-          end
-
-          return EncBody.new(params)
+          return self.new(
+            subtype: subtype,
+            flags: flags,
+            body: body,
+          )
         end
 
         def to_s()
-          if(@subtype == SUBTYPE_INIT)
-            return "[[ENC|INIT]] :: flags = 0x%04x, pubkey = %s,%s" % [@flags, CryptoHelper.bignum_to_text(@public_key_x), CryptoHelper.bignum_to_text(@public_key_y)]
-          elsif(@subtype == SUBTYPE_AUTH)
-            return "[[ENC|AUTH]] :: flags = 0x%04x, authenticator = %s" % [@flags, @authenticator.unpack("H*").pop()]
-          else
-            raise(DnscatException, "Unknown subtype: #{@subtype}")
-          end
+          return "[[ENC]] :: flags = 0x%04x %s" % [@flags, @body.to_s]
         end
 
         def to_bytes()
-          if(@subtype == SUBTYPE_INIT)
-            public_key_x = CryptoHelper.bignum_to_binary(@public_key_x)
-            public_key_y = CryptoHelper.bignum_to_binary(@public_key_y)
-
-            return [@subtype, @flags, public_key_x, public_key_y].pack("nna32a32")
-          elsif(@subtype == SUBTYPE_AUTH)
-            return [@subtype, @flags, @authenticator].pack("nna32")
-          else
-            raise(DnscatException, "Unknown subtype: #{@subtype}")
-          end
+          return [@subtype, @flags, @body.to_bytes].pack("nna*")
         end
       end
     end
