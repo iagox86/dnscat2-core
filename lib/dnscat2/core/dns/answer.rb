e83f7a3fe2bdd80ba0b5fb19542c9da8784266f0
iagox86 <ron-git@skullsecurity.org>
2017-06-22 10:49:37 -0700

    Fix up and test the Answer class

--- lib/dnscat2/core/dns/answer.rb
+++ lib/dnscat2/core/dns/answer.rb
@@ -25,26 +25,25 @@ module DNSer
 
     def self.unpack(unpacker)
       name = unpacker.unpack_name()
-      type, cls, ttl, rr_length = unpacker.unpack("nnNn")
+      type, cls, ttl = unpacker.unpack("nnN")
 
       case type
       when TYPE_A
-        rr = A.unpack(data)
+        rr = A.unpack(unpacker)
       when TYPE_NS
-        rr = NS.unpack(data)
+        rr = NS.unpack(unpacker)
       when TYPE_CNAME
-        rr = CNAME.unpack(data)
+        rr = CNAME.unpack(unpacker)
       when TYPE_SOA
-        rr = SOA.unpack(data)
+        rr = SOA.unpack(unpacker)
       when TYPE_MX
-        rr = MX.unpack(data)
+        rr = MX.unpack(unpacker)
       when TYPE_TXT
-        rr = TXT.unpack(data)
+        rr = TXT.unpack(unpacker)
       when TYPE_AAAA
-        rr = AAAA.unpack(data)
+        rr = AAAA.unpack(unpacker)
       else
-        puts("Warning: Unknown record type: #{type}")
-        rr = RRUnknown.unpack(type, data, rr_length)
+        rr = RRUnknown.unpack(unpacker, type)
       end
 
       return self.new(
@@ -57,19 +56,23 @@ module DNSer
     end
 
     def pack(packer)
+      # The name is echoed back
       packer.pack_name(@name)
 
-      # We don't actually know what the length is till after we pack a record,
-      # so use 0x1337 as a placeholder
-      packer.pack('nnNn', @type, @cls, @ttl)
+      # The type/class/ttl are added
+      packer.pack('nnN', @type, @cls, @ttl)
+
+      # Finally, the length and rr (the length is included)
       @rr.pack(packer)
     end
 
     def to_s()
-      return '%s [%s %s]' % [
+      return '%s %d [%s %s] %s' % [
         @name,
+        @ttl,
         TYPES[@type] || '<0x%04x?>' % @type,
         CLSES[@cls]  || '<0x%04x?>' % @cls,
+        @rr.to_s(),
       ]
     end
   end
