fa6b0b630892dc40b60da60f2d132976f60a8166
iagox86 <ron-git@skullsecurity.org>
2017-06-25 16:03:38 -0700

    Start fixing dnser, added a simple logging class

--- lib/dnscat2/core/dns/dnser.rb
+++ lib/dnscat2/core/dns/dnser.rb
@@ -27,8 +27,7 @@
 # To listen for queries, create a new instance of DNSer, which will begin
 # listening on a port, but won't actually handle queries yet:
 #
-#   dnser = DNSer.new("0.0.0.0", 53)
-#   dnser.on_request() do |transaction|
+#   dnser = DNSer.new("0.0.0.0", 53) do |transaction|
 #     ...
 #   end
 #
@@ -47,10 +46,75 @@ require_relative './vash'
 
 module DNSer
   class DNSer
+    attr_reader :thread
+
+    def initialize(s:, logger:, host:"0.0.0.0", port:53)
+      @s = s
+      @s.bind(host, port)
+      @logger = logger
+
+      @thread = Thread.new() do
+        begin
+          loop do
+            # Grab all the data we can off the socket
+            data = @s.recvfrom(65536)
+
+            begin
+              # Data is an array where the first element is the actual data, and the second is the host/port
+              request = Packet.parse(data[0])
+            rescue DnsException => e
+              logger.error("Failed to parse the DNS packet: %s" % e.to_s())
+              next
+            end
+
+            # Create a transaction object, which we can use to respond
+            transaction = Transaction.new(
+              s: @s,
+              request_packet: request,
+              host: data[1][3],
+              port: data[1][1],
+            )
+
+            begin
+              proc.call(transaction)
+            rescue StandardError => e
+              logger.error("Error thrown while processing the DNS packet: %s" % e.to_s())
+              logger.info(e.backtrace())
+              transaction.error!(RCODE_SERVER_FAILURE)
+            end
+          end
+        ensure
+          @s.close()
+        end
+      end
+    end
+
+    # Kill the listener
+    def stop()
+      if(@thread.nil?)
+        @logger.error("Tried to stop a listener that wasn't listening!")
+        return
+      end
+
+      @thread.kill()
+      @thread = nil
+    end
+
+    # After calling on_request(), this can be called to halt the program's
+    # execution until the thread is stopped.
+    def wait()
+      if(@thread.nil?)
+        @logger.error("Tried to wait on a DNSer instance that wasn't listening!")
+        return
+      end
+
+      @thread.join()
+    end
+
     # Send out a query, asynchronously. This immediately returns, then, when the
     # query is finished, the callback block is called with a DNSer::Packet that
     # represents the response (or nil, if there was a timeout).
-    def DNSer.query(hostname, params = {})
+    def DNSer.query(s, hostname, params = {})
       server   = params[:server]   || "8.8.8.8"
       port     = params[:port]     || 53
       type     = params[:type]     || DNSer::Packet::TYPE_A
@@ -73,7 +137,7 @@ module DNSer
         rescue Timeout::Error
           proc.call(nil)
         rescue Exception => e
-          puts("There was an exception sending a query for #{hostname} to #{server}:#{port}: #{e}")
+          @logger.error("There was an exception sending a query for #{hostname} to #{server}:#{port}: #{e}")
         ensure
           if(s)
             s.close()
@@ -81,66 +145,5 @@ module DNSer
         end
       end
     end
-  end
-
-  # Create a new DNSer and listen on the given host/port. This will throw an
-  # exception if we aren't allowed to bind to the given port.
-  def initialize(host, port)
-    @s = UDPSocket.new()
-    @s.bind(host, port)
-    @thread = nil
-  end
-
-  # This method returns immediately, but spawns a background thread. The thread
-  # will receive and unpack DNS packets, create a transaction, and pass it to
-  # the caller's block.
-  def on_request()
-    @thread = Thread.new() do |t|
-      begin
-        loop do
-          data = @s.recvfrom(65536)
-
-          # Data is an array where the first element is the actual data, and the second is the host/port
-          request = DNSer::Packet.unpack(data[0])
-
-          # Create a transaction object, which we can use to respond
-          transaction = Transaction.new(@s, request, data[1][3], data[1][1])
-
-          if(!transaction.sent)
-            begin
-              proc.call(transaction)
-            rescue StandardError => e
-              puts("Caught an error: #{e}")
-              puts(e.backtrace())
-              transaction.reply!(transaction.response_template({:rcode => DNSer::Packet::RCODE_SERVER_FAILURE}))
-            end
-          end
-        end
-      ensure
-        @s.close
-      end
-    end
-  end
-
-  # Kill the listener
-  def stop()
-    if(@thread.nil?)
-      puts("Tried to stop a listener that wasn't listening!")
-      return
-    end
-
-    @thread.kill()
-    @thread = nil
-  end
-
-  # After calling on_request(), this can be called to halt the program's
-  # execution until the thread is stopped.
-  def wait()
-    if(@thread.nil?)
-      puts("Tried to wait on a DNSer instance that wasn't listening!")
-      return
-    end
-
-    @thread.join()
   end
 end
---
+++ lib/dnscat2/core/libs/logger.rb
@@ -0,0 +1,43 @@
+# Encoding: ASCII-8BIT
+##
+# dnser.rb
+# Created Oct 7, 2015
+# By Ron Bowes
+#
+# See: LICENSE.md
+##
+
+module Dnscat2
+  LOG_LEVEL_INFO = 0
+  LOG_LEVEL_LOG = 1
+  LOG_LEVEL_WARNING = 2
+  LOG_LEVEL_ERROR = 3
+  LOG_LEVEL_FATAL = 4
+
+  class Logger
+    def initialize(level:LOG_LEVEL_WARNING, sink: STDERR)
+      @level = level
+      @sink = sink
+    end
+
+    def info(msg)
+      sink.puts(msg.to_s) if level <= LOG_LEVEL_INFO
+    end
+
+    def log(msg)
+      sink.puts(msg.to_s) if level <= LOG_LEVEL_LOG
+    end
+
+    def warning(msg)
+      sink.puts(msg.to_s) if level <= LOG_LEVEL_WARNING
+    end
+
+    def error(msg)
+      sink.puts(msg.to_s) if level <= LOG_LEVEL_ERROR
+    end
+
+    def fatal(msg)
+      sink.puts(msg.to_s) if level <= LOG_LEVEL_FATAL
+    end
+  end
+end
