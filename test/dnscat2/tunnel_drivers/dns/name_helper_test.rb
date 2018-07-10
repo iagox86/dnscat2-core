6165feff724220443eeedcfeb344cd7d6b796127
iagox86 <ron-git@skullsecurity.org>
2018-07-10 11:20:43 -0700

    Enable more name-encoding tests

--- test/dnscat2/tunnel_drivers/dns/name_helper_test.rb
+++ test/dnscat2/tunnel_drivers/dns/name_helper_test.rb
@@ -15,33 +15,34 @@ module Dnscat2
 
           def test_max_length()
             # Start with (255 - 4 periods) / 2 characters/byte => 125
-            assert_equal(125, NameHelper.new(tag: nil, domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(125, NameHelper.new(tag: nil,      domain: nil, max_subdomain_jitter: 0).max_length)
 
             # Prepending 'a.' means we have two less bytes, so (255 - 3 periods - 2 bytes) / 2 characters/byte => 125
-            assert_equal(125, NameHelper.new(tag: 'a', domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(125, NameHelper.new(tag: 'a',      domain: nil, max_subdomain_jitter: 0).max_length)
 
             # Prepending 'aa.' means we have three less bytes, so (255 - 3 periods - 3 bytes) / 2 characters/byte => 124
-            assert_equal(124, NameHelper.new(tag: 'aa', domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(124, NameHelper.new(tag: 'aa',     domain: nil, max_subdomain_jitter: 0).max_length)
 
             # (255 - 3 - 4) / 2
-            assert_equal(124, NameHelper.new(tag: 'aaa', domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(124, NameHelper.new(tag: 'aaa',    domain: nil, max_subdomain_jitter: 0).max_length)
 
             # (255 - 3 - 5) / 2
-            assert_equal(123, NameHelper.new(tag: 'aaaa', domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(123, NameHelper.new(tag: 'aaaa',   domain: nil, max_subdomain_jitter: 0).max_length)
 
             # (255 - 3 - 6) / 2
-            assert_equal(123, NameHelper.new(tag: 'aaaaa', domain: nil, max_subdomain_jitter: 0).max_length)
+            assert_equal(123, NameHelper.new(tag: 'aaaaa',  domain: nil, max_subdomain_jitter: 0).max_length)
 
             # (255 - 3 - 7) / 2
             assert_equal(122, NameHelper.new(tag: 'aaaaaa', domain: nil, max_subdomain_jitter: 0).max_length)
-#
-#            # Appending domains should be exactly the same as prepending a tag
-#            assert_equal(125, NameHelper.new(tag: nil, domain: 'a', max_subdomain_jitter: 0).max_length)
-#            assert_equal(124, NameHelper.new(tag: nil, domain: 'aa', max_subdomain_jitter: 0).max_length)
-#            assert_equal(124, NameHelper.new(tag: nil, domain: 'aaa', max_subdomain_jitter: 0).max_length)
-#            assert_equal(123, NameHelper.new(tag: nil, domain: 'aaaa', max_subdomain_jitter: 0).max_length)
-#            assert_equal(123, NameHelper.new(tag: nil, domain: 'aaaaa', max_subdomain_jitter: 0).max_length)
-#            assert_equal(122, NameHelper.new(tag: nil, domain: 'aaaaaa', max_subdomain_jitter: 0).max_length)
+
+            # Appending domains should be exactly the same as prepending a tag
+            assert_equal(125, NameHelper.new(tag: nil, domain: nil,      max_subdomain_jitter: 0).max_length)
+            assert_equal(125, NameHelper.new(tag: nil, domain: 'a',      max_subdomain_jitter: 0).max_length)
+            assert_equal(124, NameHelper.new(tag: nil, domain: 'aa',     max_subdomain_jitter: 0).max_length)
+            assert_equal(124, NameHelper.new(tag: nil, domain: 'aaa',    max_subdomain_jitter: 0).max_length)
+            assert_equal(123, NameHelper.new(tag: nil, domain: 'aaaa',   max_subdomain_jitter: 0).max_length)
+            assert_equal(123, NameHelper.new(tag: nil, domain: 'aaaaa',  max_subdomain_jitter: 0).max_length)
+            assert_equal(122, NameHelper.new(tag: nil, domain: 'aaaaaa', max_subdomain_jitter: 0).max_length)
           end
 
 #          def test_max_length_different_segment_lengths()
